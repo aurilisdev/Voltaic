6b6f5f0e6773e4a079692482817c4b17149f9d7f
skip999 <skiprocks999@gmail.com>
2025-05-13 18:59:29 -0500

    fix data issue

--- src/main/java/voltaic/datagen/utils/server/radiation/BaseRadiationShieldingProvider.java
+++ src/main/java/voltaic/datagen/utils/server/radiation/BaseRadiationShieldingProvider.java
@@ -15,14 +15,14 @@ import java.util.concurrent.CompletableFuture;
 
 public abstract class BaseRadiationShieldingProvider implements DataProvider {
 
-    public static final String LOC = "data/" + Voltaic.ID + "/" + RadiationShieldingRegister.FOLDER + "/" + RadiationShieldingRegister.FILE_NAME;
-
     private final PackOutput output;
     private final String modID;
+    private final String loc;
 
     public BaseRadiationShieldingProvider(PackOutput output, String modID) {
         this.output = output;
         this.modID = modID;
+        loc = "data/" + Voltaic.ID + "/" + RadiationShieldingRegister.FOLDER + "/" + modID + "_" + RadiationShieldingRegister.FILE_NAME;
     }
 
     @Override
@@ -30,7 +30,7 @@ public abstract class BaseRadiationShieldingProvider implements DataProvider {
         JsonObject json = new JsonObject();
         getRadiationShielding(json);
 
-        Path parent = output.getOutputFolder().resolve(LOC + ".json");
+        Path parent = output.getOutputFolder().resolve(loc + ".json");
 
         return CompletableFuture.allOf(DataProvider.saveStable(cache, json, parent));
     }
