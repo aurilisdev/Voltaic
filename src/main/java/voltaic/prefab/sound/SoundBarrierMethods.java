60d78b676b5c10ea7b57d918179bb672bae78c42
skip999 <skiprocks999@gmail.com>
2025-07-07 22:06:12 -0500

    tweaks

--- src/main/java/voltaic/prefab/sound/SoundBarrierMethods.java
+++ src/main/java/voltaic/prefab/sound/SoundBarrierMethods.java
@@ -11,8 +11,16 @@ public class SoundBarrierMethods {
 		Minecraft.getInstance().getSoundManager().play(new TickableSoundTile<>(event, tile, repeat));
 	}
 
+	public static <T extends BlockEntity & ITickableSound> void playTileSound(SoundEvent event, T tile, double range, boolean repeat) {
+		Minecraft.getInstance().getSoundManager().play(new TickableSoundTile<>(event, tile, range, repeat));
+	}
+
 	public static <T extends BlockEntity & ITickableSound> void playTileSound(SoundEvent event, SoundSource source, T tile, float volume, float pitch, boolean repeat) {
 		Minecraft.getInstance().getSoundManager().play(new TickableSoundTile<>(event, source, tile, volume, pitch, repeat));
+	}
+
+	public static <T extends BlockEntity & ITickableSound> void playTileSound(SoundEvent event, SoundSource source, T tile, float volume, float pitch, double range, boolean repeat) {
+		Minecraft.getInstance().getSoundManager().play(new TickableSoundTile<>(event, source, tile, volume, pitch, range, repeat));
 	}
 
 }
