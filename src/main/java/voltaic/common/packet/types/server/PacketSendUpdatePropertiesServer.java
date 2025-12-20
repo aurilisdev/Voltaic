d8dec21b0ccaef6681f5d41f8994450e73e56da0
aurilisdev <aurilisdev@gmail.com>
2025-12-20 23:19:09 +0100

    Fix possible incompatibility

--- src/main/java/voltaic/common/packet/types/server/PacketSendUpdatePropertiesServer.java
+++ src/main/java/voltaic/common/packet/types/server/PacketSendUpdatePropertiesServer.java
@@ -22,7 +22,7 @@ public class PacketSendUpdatePropertiesServer implements CustomPacketPayload {
             //buf.writeResourceLocation(packet.wrapper.type().getId());
             //packet.wrapper.type().getPacketCodec().encode(buf, packet.wrapper.value());
 
-            buf.writeNbt(packet.data);
+            buf.writeNbt(packet.data == null ? new CompoundTag() : packet.data);
             buf.writeInt(packet.index);
             buf.writeBlockPos(packet.tilePos);
 
