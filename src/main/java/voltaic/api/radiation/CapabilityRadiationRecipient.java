e9260b85c0872f08e3d84bc02bbd1be49ab503e2
aurilisdev <aurilisdev@gmail.com>
2026-06-13 12:22:22 +0200

    Fix Nuclear Science #185: Instant death of radiation while mining - patch 3
    Re: https://github.com/aurilisdev/Nuclear-Science/issues/185

--- src/main/java/voltaic/api/radiation/CapabilityRadiationRecipient.java
+++ src/main/java/voltaic/api/radiation/CapabilityRadiationRecipient.java
@@ -157,7 +157,7 @@ public class CapabilityRadiationRecipient implements IRadiationRecipient, ICapab
 	}
 
 	public static int getDurationFromRadiation(double radiation) {
-		return (int) Math.max(20.0, radiation / 100.0 * 20.0);
+        return (int) Math.max(1, radiation / 100.0 * 20.0);
 	}
 
 	public static int getAmplitudeFromRadiation(double radiation, double strength) {
