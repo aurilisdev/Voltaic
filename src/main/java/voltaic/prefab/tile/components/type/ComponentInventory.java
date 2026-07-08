b89ef3de686d6d372d87e493e7acf78de36315a9
aurilisdev <aurilisdev@gmail.com>
2026-07-08 19:49:35 +0200

    Patch 9: Fix #100 and reveal security breach...

--- src/main/java/voltaic/prefab/tile/components/type/ComponentInventory.java
+++ src/main/java/voltaic/prefab/tile/components/type/ComponentInventory.java
@@ -133,7 +133,7 @@ public class ComponentInventory implements IComponent, WorldlyContainer {
 	    items.add(ItemStack.EMPTY);
 	}
 
-	this.items = holder.property(new ListProperty<>(PropertyTypes.ITEM_STACK_LIST, "machineinventory", items));
+	this.items = holder.property(new ListProperty<>(PropertyTypes.ITEM_STACK_LIST, "machineinventory", items)).setNoUpdateServer();
 
     }
 
@@ -348,7 +348,14 @@ public class ComponentInventory implements IComponent, WorldlyContainer {
 
     @Override
     public void clearContent() {
-	items.wipeList();
+	List<ItemStack> cleared = new ArrayList<>(inventorySize);
+
+	for (int i = 0; i < inventorySize; i++) {
+	    cleared.add(ItemStack.EMPTY);
+	}
+
+	items.setValue(cleared);
+	setChanged(-1);
     }
 
     @Override
